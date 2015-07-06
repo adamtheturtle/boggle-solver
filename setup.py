a32cce0f6af3d429c17a4d38271d717e0b464881
Adam Dangoor <adamdangoor@gmail.com>
2015-07-06 19:13:27 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import os.path
+
+description = file(os.path.join(os.path.dirname(__file__), 'README.md'), 'rb').read()
+
+
+setup(
+    name="bogglesolver",
+    version="1.0.0",
+    description="Solve a game of Boggle.",
+    long_description=description,
+    author="Adam Dangoor",
+    author_email="adamdangoor@gmail.com",
+    install_requires=[],
+    zip_safe=True,
+    packages=find_packages('.'),
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        ],
+)
