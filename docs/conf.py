edb1b2d6f7b6f24e7331084ef165e06566d7aac7
Adam Dangoor <adamdangoor@gmail.com>
2015-07-26 09:13:58 +0100

    Start of autodocumenting

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,15 @@ import shlex
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+sys.path.insert(0, os.path.abspath("../.."))
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+]
+
+autodoc_default_flags = ['members']
+autosummary_generate = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
