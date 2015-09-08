6b499b18dc0a25de9d9fd72495616a2c2842b351
Adam Dangoor <adamdangoor@gmail.com>
2015-09-08 13:26:15 +0100

    Remove rubbish

--- boggle/docs/source/conf.py
+++ boggle/docs/source/conf.py
@@ -29,8 +29,6 @@ import shlex
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-current_path = __file__
-# import pdb; pdb.set_trace()
 sys.path.insert(0, os.path.abspath('..'))
 extensions = [
     'boggle.task.task'
